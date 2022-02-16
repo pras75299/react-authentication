809e542fa72803306f98f9fa34f484d5f0e01d10
pras75299 <singh.prashantking@gmail.com>
2022-02-17 00:02:51 +0530

    Login functionality add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-  return <button type="button">Login</button>;
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <button onClick={() => loginWithRedirect()}>Login</button>
+    )
+  );
 }
 
 export default Login;
