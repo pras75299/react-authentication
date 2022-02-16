98f86fab7616fd1b847aa72ed822c02fa5613b8c
pras75299 <singh.prashantking@gmail.com>
2022-02-16 23:55:03 +0530

    create login logout and profile component add into app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Login from "./components/Login/Login";
+import Logout from "./components/Logout/Logout";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
       <h1>Auth Practice Project</h1>
+      <Login />
+      <Logout />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Logout/Logout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Logout() {
+  return <button type="button">Logout</button>;
+}
+
+export default Logout;
