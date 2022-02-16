445b6a2538b75c5fdcc2e9edbb527da399a4e313
pras75299 <singh.prashantking@gmail.com>
2022-02-17 00:03:24 +0530

    Logout functionality add

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import { useAuth0 } from "@auth0/auth0-react";
 function Logout() {
-  return <button type="button">Logout</button>;
+  const { logout, isAuthenticated } = useAuth0();
+  return isAuthenticated && <button onClick={() => logout()}>Logout</button>;
 }
 
 export default Logout;
