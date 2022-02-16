bf9fb844aa3a55ac7550af1e6d3c5555527a7e02
pras75299 <singh.prashantking@gmail.com>
2022-02-17 00:04:15 +0530

    add profile functionality conditionally

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { user, isAuthenticated } = useAuth0();
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <p>Mail id- {user.email}</p>
+        {/* <div>{JSON.stringify(user, null, 2)}</div> */}
+      </div>
+    )
+  );
 }
 
 export default Profile;
